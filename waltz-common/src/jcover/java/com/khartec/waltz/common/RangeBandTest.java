089a48f6536f81e718e2b010d0a902cb48ff9f3b
Thomas Perkins <thomasperkins1123@gmail.com>
2020-03-04 11:08:37 +0000

    Add tests for common Coverage: 68.8%

---
+++ waltz-common/src/jcover/java/com/khartec/waltz/common/RangeBandTest.java
@@ -0,0 +1,12 @@
+package com.khartec.waltz.common;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for com.khartec.waltz.common.RangeBand
+ *
+ * @author Diffblue JCover
+ */
+
+public class RangeBandTest {
+}
